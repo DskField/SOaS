5b558053eaac029e169d54f12aeefe5937018205
InOurBones <tomlagerweij@outlook.com>
2019-04-17 17:21:15 +0200

    Setup
    finished GameColor
    finished Card
    finished PersonalGoalCard
    finished CollectiveGoalCard
    finished ToolCard
    finished CurrencyStone

--- src/game/CurrencyStone.java
+++ src/game/CurrencyStone.java
@@ -2,4 +2,13 @@ package game;
 
 public class CurrencyStone {
 
+	private int stoneID;
+	
+	public CurrencyStone(int stoneID) {
+		this.stoneID = stoneID;
+	}
+	
+	public int getStoneID() {
+		return stoneID;
+	}
 }
--- src/game/ToolCard.java
+++ src/game/ToolCard.java
@@ -1,5 +1,30 @@
 package game;
 
-public class ToolCard {
+import java.util.ArrayList;
 
+public class ToolCard extends Card {
+
+	private int cardNumber;
+	private ArrayList<CurrencyStone> usedCurrencyStones;
+	
+	public ToolCard(String cardName, String cardDescription, int cardNumber) {
+		super(cardName, cardDescription);
+		this.cardNumber = cardNumber;
+		usedCurrencyStones = new ArrayList<CurrencyStone>();
+	}
+
+	public int getCardNumber() {
+		return cardNumber;
+	}
+	
+	// you return the whole list, because you can get the size with it and
+	// know which CurrencyStone is currently on the ToolCard
+	public ArrayList<CurrencyStone> getUsedCurrencyStones() {
+		return usedCurrencyStones;
+	}
+	
+	// TODO: check logic of following method
+	public void payToolCard(CurrencyStone currenctStone) {
+		usedCurrencyStones.add(currenctStone);
+	}
 }
