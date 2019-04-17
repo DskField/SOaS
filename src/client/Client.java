d578df6b0a4fb35a8f31c9772772054f3221dd63
David <davidkvelderman@gmail.com>
2019-04-17 21:00:32 +0200

    Client package first steps

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,5 +1,45 @@
 package client;
 
-public class Client {
+import java.util.ArrayList;
 
+// Importeer Player uit de Game package
+import game.Player;
+
+public class Client {
+	
+	private ArrayList<Lobby> lobbies;
+
+	public void acceptChallenge() {
+
+	}
+
+	public void denyChallenge() {
+
+	}
+
+	public void inviteUser(Player player) {
+
+		// Grijp hoogste GameID van de database, voeg 1 toe
+		// GameID is hier dus dummy data
+		// In het eindproduct moeten we hier een getal aan zien te hangen door een connectie te maken met een database
+		
+		// ... We gaan ervanuit dat hij hier de waarde uit de database heeft gehaald ...
+		int dbGameID = 0; // Stel waarde in zodat de compiler niet zeurt
+		
+		// Maakt nieuwe lobby aan
+		
+		lobbies.add(new Lobby(dbGameID));
+		
+	}
+
+	public void updateClient() {
+
+	}
+
+	public String findUser(User player) {
+
+		String username = player.getInlognaam();
+
+		return username;
+	}
 }
