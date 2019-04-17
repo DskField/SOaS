f296a4e94ce9d40d63908ed6ae97357d4dc4f223
JustCrescent <49680733+JustCrescent@users.noreply.github.com>
2019-04-17 22:11:45 +0200

    finished SpaceGlass

--- src/game/SpaceGlass.java
+++ src/game/SpaceGlass.java
@@ -1,5 +1,24 @@
 package game;
 
-public class SpaceGlass {
+public class SpaceGlass extends Space{
 
+	private Die die;
+	
+	public SpaceGlass(int x, int y) {
+		super(x, y);
+		die = null;
+	}
+
+	public void placeDie(Die die) {
+		try {
+			this.die = die;
+		}
+		catch(Exception e) {
+			System.out.println("ERROR: " + e.getMessage());
+		}
+	}
+	
+	public Die getDie() {
+		return die;
+	}
 }
