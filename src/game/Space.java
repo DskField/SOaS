7df56746266dc84ee2698fdfb32e105045825289
JustCrescent <49680733+JustCrescent@users.noreply.github.com>
2019-04-17 20:23:37 +0200

    finished Space

--- src/game/Space.java
+++ src/game/Space.java
@@ -2,4 +2,25 @@ package game;
 
 public abstract class Space {
 
+  private int spaceId;
+	private int xCor;
+	private int yCor;
+	
+	public Space(int id, int x, int y) {
+		spaceId = id;
+		xCor = x;
+		yCor = y;
+	}
+	
+	public int getSpaceId() {
+		return spaceId;
+	}
+	
+	public int getXCor() {
+		return xCor;
+	}
+	
+	public int getYCor() {
+		return yCor;
+	}
 }
