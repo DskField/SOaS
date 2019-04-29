79aa09dcbf84aadab70be168d6f0e6810d483b0d
David <davidkvelderman@gmail.com>
2019-04-29 13:15:15 +0200

    Update ClientDAO.java

--- src/database/ClientDAO.java
+++ src/database/ClientDAO.java
@@ -14,21 +14,5 @@ public class ClientDAO extends BaseDAO {
 
 	Connection con = super.getConnection();
 	
-	private List<Game> selectGame(String query) {
-		List<Game> results = new ArrayList<Game>();
-		try {
-			PreparedStatement stmt = con.prepareStatement(query);
-			ResultSet dbResultSet = stmt.executeQuery();
-			con.commit();
-			while (dbResultSet.next()) {
-				int gameID = dbResultSet.getInt("idgame");
-				Game game = new Game(gameID);
-				results.add(game);
-			}
-			stmt.close();
-		} catch (SQLException e) {
-			System.err.println("CollectiveGoalCardDAO " + e.getMessage());
-		}
-		return results;
-	}
+	 
 }
