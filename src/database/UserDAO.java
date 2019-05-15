4dbec8ab99b69137436851149c80422c59f4eb4e
InOurBones <tomlagerweij@outlook.com>
2019-05-15 22:12:06 +0200

    optimized UserDAO
    changed the code to get the MostPlacedValue and MostPlacedColor to optimize the readability and useability of the code

--- src/database/UserDAO.java
+++ src/database/UserDAO.java
@@ -41,82 +41,24 @@ class UserDAO {
 			stmt.close();
 
 			// MostPlacedColor
-			// TODO review this, don't know how to do this more efficient
-			GameColor mostPlacedColor = GameColor.EMPTY;
-			int totalPlacedColor = 0;
-			PreparedStatement stmtMostPlacedColor = con
-					.prepareStatement("SELECT SUM(CASE WHEN diecolor = \"rood\" THEN 1 ELSE 0 END) AS totalred,\r\n"
-							+ "SUM(CASE WHEN diecolor = \"geel\" THEN 1 ELSE 0 END) AS totalyellow,\r\n"
-							+ "SUM(CASE WHEN diecolor = \"groen\" THEN 1 ELSE 0 END) AS totalgreen,\r\n"
-							+ "SUM(CASE WHEN diecolor = \"blauw\" THEN 1 ELSE 0 END) AS totalblue,\r\n"
-							+ "SUM(CASE WHEN diecolor = \"paars\" THEN 1 ELSE 0 END) AS totalpurple\r\n"
-							+ "FROM playerframefield\r\n" + "JOIN player ON player_idplayer = idplayer\r\n"
-							+ "WHERE dienumber IS NOT NULL AND username = ?");
+			PreparedStatement stmtMostPlacedColor = con.prepareStatement("SELECT diecolor FROM player AS p1"
+					+ "LEFT JOIN playerframefield AS p2 ON p1.idplayer = p2.player_idplayer"
+					+ "WHERE username = ? GROUP BY diecolor ORDER BY COUNT(diecolor) DESC LIMIT 1");
 			stmtMostPlacedColor.setString(1, username);
 			ResultSet dbResultSetMostPlacedColor = stmtMostPlacedColor.executeQuery();
 			dbResultSetMostPlacedColor.next();
-			if (dbResultSetMostPlacedColor.getInt("totalred") > totalPlacedColor) {
-				totalPlacedColor = dbResultSetMostPlacedColor.getInt("totalred");
-				mostPlacedColor = GameColor.RED;
-			}
-			if (dbResultSetMostPlacedColor.getInt("totalyellow") > totalPlacedColor) {
-				totalPlacedColor = dbResultSetMostPlacedColor.getInt("totalyellow");
-				mostPlacedColor = GameColor.YELLOW;
-			}
-			if (dbResultSetMostPlacedColor.getInt("totalgreen") > totalPlacedColor) {
-				totalPlacedColor = dbResultSetMostPlacedColor.getInt("totalgreen");
-				mostPlacedColor = GameColor.GREEN;
-			}
-			if (dbResultSetMostPlacedColor.getInt("totalblue") > totalPlacedColor) {
-				totalPlacedColor = dbResultSetMostPlacedColor.getInt("totalblue");
-				mostPlacedColor = GameColor.BLUE;
-			}
-			if (dbResultSetMostPlacedColor.getInt("totalpurple") > totalPlacedColor) {
-				totalPlacedColor = dbResultSetMostPlacedColor.getInt("totalpurple");
-				mostPlacedColor = GameColor.PURPLE;
-			}
+			GameColor mostPlacedColor = GameColor.getEnum(dbResultSetMostPlacedColor.getString("diecolor"));
 			stmtMostPlacedColor.close();
 
 			// MostPlacedValue
-			// TODO review this, don't know how to do this more efficient
-			int mostPlacedValue = 0;
-			int totalPlacedValue = 0;
-			PreparedStatement stmtMostPlacedValue = con
-					.prepareStatement("SELECT SUM(CASE WHEN dienumber = 1 THEN 1 ELSE 0 END) AS totalone,\r\n"
-							+ "SUM(CASE WHEN dienumber = 2 THEN 1 ELSE 0 END) AS totaltwo,\r\n"
-							+ "SUM(CASE WHEN dienumber = 3 THEN 1 ELSE 0 END) AS totalthree,\r\n"
-							+ "SUM(CASE WHEN dienumber = 4 THEN 1 ELSE 0 END) AS totalfour,\r\n"
-							+ "SUM(CASE WHEN dienumber = 5 THEN 1 ELSE 0 END) AS totalfive,\r\n"
-							+ "SUM(CASE WHEN dienumber = 6 THEN 1 ELSE 0 END) AS totalsix\r\n"
-							+ "FROM playerframefield\r\n" + "JOIN player ON player_idplayer = idplayer\r\n"
-							+ "WHERE dienumber IS NOT NULL AND username = ?");
+			PreparedStatement stmtMostPlacedValue = con.prepareStatement("SELECT eyes"
+					+ "FROM player AS p1 LEFT JOIN playerframefield AS p2 ON p1.idplayer = p2.player_idplayer"
+					+ "LEFT JOIN gamedie AS gd ON p2.idgame = gd.idgame AND p2.dienumber = gd.dienumber AND p2.diecolor = gd.diecolor"
+					+ "WHERE username = 'speler3' GROUP BY eyes ORDER BY COUNT(eyes) DESC LIMIT 1");
 			stmtMostPlacedValue.setString(1, username);
 			ResultSet dbResultSetMostPlacedValue = stmtMostPlacedValue.executeQuery();
 			dbResultSetMostPlacedValue.next();
-			if (dbResultSetMostPlacedValue.getInt("totalone") > totalPlacedValue) {
-				totalPlacedValue = dbResultSetMostPlacedValue.getInt("totalone");
-				mostPlacedValue = 1;
-			}
-			if (dbResultSetMostPlacedValue.getInt("totaltwo") > totalPlacedValue) {
-				totalPlacedValue = dbResultSetMostPlacedValue.getInt("totaltwo");
-				mostPlacedValue = 2;
-			}
-			if (dbResultSetMostPlacedValue.getInt("totalthree") > totalPlacedValue) {
-				totalPlacedValue = dbResultSetMostPlacedValue.getInt("totalthree");
-				mostPlacedValue = 3;
-			}
-			if (dbResultSetMostPlacedValue.getInt("totalfour") > totalPlacedValue) {
-				totalPlacedValue = dbResultSetMostPlacedValue.getInt("totalfour");
-				mostPlacedValue = 4;
-			}
-			if (dbResultSetMostPlacedValue.getInt("totalfive") > totalPlacedValue) {
-				totalPlacedValue = dbResultSetMostPlacedValue.getInt("totalfive");
-				mostPlacedValue = 5;
-			}
-			if (dbResultSetMostPlacedValue.getInt("totalsix") > totalPlacedValue) {
-				totalPlacedValue = dbResultSetMostPlacedValue.getInt("totalsix");
-				mostPlacedValue = 6;
-			}
+			int mostPlacedValue = dbResultSetMostPlacedValue.getInt("eyes");
 			stmtMostPlacedValue.close();
 
 			result = new User(gamesPlayed, maxScore, mostPlacedColor, mostPlacedValue);
