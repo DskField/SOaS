68204135eb24386dc897019a327de61b0edd22e7
InOurBones <tomlagerweij@outlook.com>
2019-06-01 09:35:10 +0200

    Import fix

--- src/view/LobbyListPane.java
+++ src/view/LobbyListPane.java
@@ -14,7 +14,6 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
