d00898710bfc1c8c5b245ae69c77b7e1b3c34a0c
Adri van Kempen <adrivankempen@gmail.com>
2019-06-03 10:52:54 +0200

    Fix 10.52
    Text verbeteren in choice scene

--- src/view/ChoiceScene.java
+++ src/view/ChoiceScene.java
@@ -7,6 +7,7 @@ import game.PatternCard;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Background;
@@ -26,7 +27,7 @@ public class ChoiceScene extends Scene {
 	/* CONSTANTS */
 	private final int generalSpacing = 10;
 	private final Font font = Font.font("arial", 25);
-	
+
 	/* VARIABLES */
 	private BorderPane root;
 	private HBox patternCardBox;
@@ -40,7 +41,7 @@ public class ChoiceScene extends Scene {
 		root = new BorderPane();
 		cardBox = new VBox();
 		PersonalGoalCardPane personalGoalCardPane = new PersonalGoalCardPane();
-		
+
 		//handles makeup
 		cardBox.setSpacing(generalSpacing);
 		cardBox.setAlignment(Pos.CENTER);
@@ -49,17 +50,22 @@ public class ChoiceScene extends Scene {
 		patternCardBox.setAlignment(Pos.CENTER);
 		patternCardBox.setSpacing(generalSpacing);
 		root.setBackground(new Background(new BackgroundFill(Color.ALICEBLUE, null, null)));
-		
-		
-		cardBox.getChildren().addAll(patternCardBox, personalGoalCardPane);
+
+		Button menuButton = new Button("Terug naar het menu");
+		menuButton.setOnAction(e -> gameController.returnToClient());
+
+		cardBox.getChildren().addAll(patternCardBox, personalGoalCardPane, menuButton);
 		root.setCenter(cardBox);
 		createPatternCards(patternCards);
 		setRoot(root);
 	}
 
 	/**
-	 * This {@code ArrayList<PatternCard>} will be shown to the user to choose his {@code PatternCard} from.
-	 * @param patternCards - {@code ArrayList<PatterCard>} that will be added to the {@code ChoiceScene}.
+	 * This {@code ArrayList<PatternCard>} will be shown to the user to choose his {@code PatternCard}
+	 * from.
+	 * 
+	 * @param patternCards - {@code ArrayList<PatterCard>} that will be added to the
+	 * {@code ChoiceScene}.
 	 */
 	private void createPatternCards(ArrayList<PatternCard> patternCards) {
 		for (PatternCard patternCard : patternCards) {
@@ -68,29 +74,28 @@ public class ChoiceScene extends Scene {
 			Label name = new Label(patternCard.getName());
 			Label difficulity = new Label("" + patternCard.getDifficulty());
 			FieldPane pc = new FieldPane(patternCard, gameController);
-			
-			
+
 			vBox.getChildren().addAll(name, pc, difficulity);
 			vBox.setAlignment(Pos.CENTER);
 			patternCardBox.getChildren().add(vBox);
-			
+
 			//disables the spaces so that they are not clickable.
 			pc.disablesSpaces();
-			
+
 			//handle event when the PatterCard is clicked.
 			pc.setOnMouseClicked(new EventHandler<MouseEvent>() {
 				@Override
 				public void handle(MouseEvent event) {
 					//handles visual feed back that the user has selected their PatternCard.
 					pc.setBorder(new Border(new BorderStroke(Color.RED, BorderStrokeStyle.SOLID, null, new BorderWidths(5))));
-					Label selectedLabel = new Label("u heeft uw kaart geselecteerd. wachten op andere spelers.");
+					Label selectedLabel = new Label("U heeft uw kaart geselecteerd. Wachten op andere spelers.");
 					selectedLabel.setTextFill(Color.RED);
 					selectedLabel.setFont(font);
 					cardBox.getChildren().add(selectedLabel);
-					
+
 					//disables the the root pane to prevent the user from interacting any further with the pane.
 					root.setDisable(true);
-					
+
 					//gives the chosen PatternCard to the GameController for further handling.
 					for (PatternCard patternCard : gameController.getPatternChoices()) {
 						if (patternCard.getPatternCardId() == pc.getPatternCardID()) {
